8c883b41153788cdbc4eab798a26b2d741c70759
gleesonmark <35773704+gleesonmark@users.noreply.github.com>
2018-01-24 16:06:41 +0000

    Update Week1.R

--- Week1.R
+++ Week1.R
@@ -1,4 +1,7 @@
 install.packages("dplyr")
 library(dplyr)
 sessionInfo()
-mtcarsmtcars %>% head ()also : dim ()dim()also:dim()mtcars %>% dim()mtcars %>% nrow()mtcars %>% ncol()mtcars %>% names()mtcars %>% class()mtcars %>% summary()iris %>% summary()library(dplyr)iris %>% glimpse()filter(iris, species == "Setosa") iris2 %>% dim()iris3 <- iris %>% filter(Sepal.Legth > 2)iris3 %>% dim()iris3 <- iris %>% filter(Sepal.Legth > 3)iris3 %>% dim()iris3 <- iris %>% filter(Sepal.Legth > 5)iris3 %>% dim()iris3 <- iris %>% filter(Sepal.Legth > 5.2)iris3 %>% dim()iris %>% filter(Sepal.Length > 5) %>% dim()iris %>% filter(Sepal.Length > 5.2) %>% dim()iris %>% filter(Sepal.Length > 5.4) %>% dim()iris %>% filter(Sepal.Length > 5.6) %>% dim()history(60)
+mtcars
+mtcars %>% head ()
+also : dim ()
+dim()also:dim()mtcars %>% dim()mtcars %>% nrow()mtcars %>% ncol()mtcars %>% names()mtcars %>% class()mtcars %>% summary()iris %>% summary()library(dplyr)iris %>% glimpse()filter(iris, species == "Setosa") iris2 %>% dim()iris3 <- iris %>% filter(Sepal.Legth > 2)iris3 %>% dim()iris3 <- iris %>% filter(Sepal.Legth > 3)iris3 %>% dim()iris3 <- iris %>% filter(Sepal.Legth > 5)iris3 %>% dim()iris3 <- iris %>% filter(Sepal.Legth > 5.2)iris3 %>% dim()iris %>% filter(Sepal.Length > 5) %>% dim()iris %>% filter(Sepal.Length > 5.2) %>% dim()iris %>% filter(Sepal.Length > 5.4) %>% dim()iris %>% filter(Sepal.Length > 5.6) %>% dim()history(60)
